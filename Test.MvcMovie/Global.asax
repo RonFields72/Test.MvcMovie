6033c87cdd3c924c93002cdefc419b76f865bf15
RonFieldsUBH <ron.fields@ubh.com>
2014-06-02 09:05:21 -0500

    Initial commit.

---
+++ Test.MvcMovie/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Test.MvcMovie.MvcApplication" Language="C#" %>
---
+++ Test.MvcMovie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Test.MvcMovie.Startup))]
+namespace Test.MvcMovie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
